06f8f8fc5d3d1c1d4dcbbb0f02a27bf32f14583a
ankit goel <ankitgoel2009@gmail.com>
2022-12-17 13:21:07 +0530

    working for datagridview

---
+++ Datagridview/textBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datagridview
+{
+    public partial class textBox : TextBox
+    {
+        public textBox()
+        {
+            InitializeComponent();
+        }
+        int m = 1;
+        protected override void OnTextChanged(EventArgs e)
+        {
+            m++;
+            MessageBox.Show(m.ToString());
+            base.OnTextChanged(e);
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        { 
+            m++;
+            MessageBox.Show(m.ToString());
+                
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+           
+            base.OnPaint(pe);
+        }
+
+      
+    }
+}
